55c2fa9b1c652fe77fc438fef797cb870a619a2b
Siddhant Prateek <siddhantprateek@gmail.com>
2021-11-01 01:49:58 +0530

    added server-side configuration

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { CLIENT_ID, DOMAIN } from './auth/CREDENTIALS';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Auth0Provider
+    domain={DOMAIN}
+    clientId={CLIENT_ID}
+    redirectUri={window.location.origin}
+    >
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
