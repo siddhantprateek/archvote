36f2f3aab3c1b4aa31557cbc9f2078437d0f9086
Siddhant Prateek <siddhantprateek@gmail.com>
2021-11-02 21:02:51 +0530

    added new component `dashboard`

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import Join from './pages/join/join.pages';
 import Contact from './pages/contact/contact.pages';
 import Home from './pages/home/home.pages';
-
+import Dashboard from './pages/dashboard/dashboard.pages';
 class App extends React.Component {
   render(){
     return (
@@ -14,6 +14,7 @@ class App extends React.Component {
 
         <Switch>
           <Route exact path="/" component={Home}/>
+          <Route exact path="/dashboard" component={Dashboard}/>
           <Route exact path="/join" component={Join} />
           <Route exact path="/contact" component={Contact} />
         </Switch>
---
+++ client/src/pages/dashboard/dashboard.pages.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BadgeList } from '../../components/badge-list/badge-list.component';
+
+class Dashboard extends React.Component{
+    render(){
+        return(
+            <div>
+                <BadgeList />
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
