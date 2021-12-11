538ea867218513a919767e8ebfcf7563d2d0b197
Siddhant Prateek <siddhantprateek@gmail.com>
2021-12-11 11:30:03 +0530

    backend branch

---
+++ server/models/PersonModel.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    fields:{
+        name    : "text",
+        surname : "text",
+        age     : "int",
+        created : "timestamp"
+    },
+    key:["name"]
+}
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@
 const express = require('express');
 const app = new express();
 const port = process.env.PORT || 4000;
-
+var models = require('express-cassandra');
 
 // Send the text message.
 // client.messages.create({
@@ -14,6 +14,33 @@ const port = process.env.PORT || 4000;
 //   from: 'YOUR_TWILIO_NUMBER',
 //   body: 'Hello from Twilio!'
 // });
+
+models.setDirectory( __dirname + '/models').bind(
+  {
+      clientOptions: {
+          contactPoints: ['127.0.0.1'],
+          localDataCenter: 'dc1',
+          protocolOptions: { port: 9042 },
+          keyspace: 'mykeyspace',
+          queryOptions: {consistency: models.consistencies.one}
+      },
+      ormOptions: {
+          defaultReplicationStrategy : {
+              class: 'SimpleStrategy',
+              replication_factor: 1
+          },
+          migration: 'safe'
+      }
+  },
+  function(err) {
+      if(err) throw err;
+
+      // You'll now have a `person` table in cassandra created against the model
+      // schema you've defined earlier and you can now access the model instance
+      // in `models.instance.Person` object containing supported orm operations.
+  }
+);
+
 
 app.listen(port, () => 
 console.log(`Listening on port ${port}`))
